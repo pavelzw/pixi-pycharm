ad0b1b6b4b2b02d87a1b6478298c653693587ae4
Pavel Zwerschke <pavelzw@gmail.com>
2024-03-12 10:11:26 +0100

    Move file to libexec in pixi env, create LICENSE (#1)

--- tests.py
+++ tests.py
@@ -3,13 +3,25 @@ import locale
 import os
 import subprocess
 import sys
+from pathlib import Path
 
 import pytest
 
 
 def run_conda(args):
     return (
-        subprocess.check_output([sys.executable, f"{os.getcwd()}/.pixi/conda"] + args)
+        subprocess.check_output(
+            [
+                sys.executable,
+                Path.cwd()
+                / ".pixi"
+                / "envs"
+                / os.environ["PIXI_ENVIRONMENT_NAME"]
+                / "libexec"
+                / "conda",
+                *args,
+            ],
+        )
         .decode(locale.getpreferredencoding())
         .rstrip()
     )
@@ -26,7 +38,9 @@ def test_info_envs_json():
     assert set(data.keys()) == {"envs_dirs", "conda_prefix", "envs"}
     assert data["envs_dirs"] == [f"{os.getcwd()}/.pixi"]
     assert data["conda_prefix"] == f"{os.getcwd()}/.pixi"
-    assert data["envs"] == [f"{os.getcwd()}/.pixi/envs/{env}" for env in ["default", "py39", "py310", "py311", "py312"]]
+    assert data["envs"] == [
+        f"{os.getcwd()}/.pixi/envs/{env}" for env in ["default", "py39", "py310", "py311", "py312"]
+    ]
 
 
 @pytest.mark.skip("Not implemented yet")
@@ -34,9 +48,9 @@ def test_list_old():
     result = run_conda(["list", "-n", "base"])
     result_e = run_conda(["list", "-n", "base", "-e"])
     ref = subprocess.check_output(["micromamba", "list", "-n", "base"]).strip()
-    ref = "\n".join(l.strip() for l in ref.splitlines()[4:])
+    ref = "\n".join(line.strip() for line in ref.splitlines()[4:])
     assert result == ref
-    assert result_e == "\n".join("=".join(l.split()[:-1]) for l in ref.splitlines())
+    assert result_e == "\n".join("=".join(line.split()[:-1]) for line in ref.splitlines())
 
 
 @pytest.mark.parametrize("env", ["default", "py39", "py310", "py311", "py312"])
@@ -47,7 +61,19 @@ def test_list(env: str):
 
 @pytest.mark.parametrize("env", ["default", "py39", "py310", "py311", "py312"])
 def test_run(env: str):
-    assert "42" == run_conda(["run", "-p", f"{os.getcwd()}/.pixi/envs/{env}", "--no-capture-output", "echo", "42"])
+    assert (
+        run_conda(
+            [
+                "run",
+                "-p",
+                f"{os.getcwd()}/.pixi/envs/{env}",
+                "--no-capture-output",
+                "echo",
+                "42",
+            ],
+        )
+        == "42"
+    )
 
 
 @pytest.mark.skip("Not implemented yet")
